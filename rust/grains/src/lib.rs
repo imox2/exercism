566b8dd784f0b044355a897873fc93a5be177323
Talha <talhaanwar.anwar@gmail.com>
2021-01-26 07:14:53 +0530

    Calculate the number of grains of wheat on a chessboard given that the number on each square doubles.(Refactored)

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,24 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    let base: u64 = 2; // an explicit type is required
-    let result: u64;
-    if s > 0 && s < 65 {
-        if s == 1 {
-            result = u64::from(s);
-        } else {
-            result = base.pow(s - 1);
-        }
-    } else {
-        panic!("Square must be between 1 and 64");
-    }
-
-    result
+    assert!((1..=64).contains(&s), "Square must be between 1 and 64");
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
     let mut total: u64 = 0;
-    let base: u64 = 2; // an explicit type is required
     for n in 1..65 {
-        total += base.pow(n - 1);
+        total += square(n);
     }
     total
 }
