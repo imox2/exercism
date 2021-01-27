e6f4407db1d27945ebf778d2dc2b2a50832b5bbd
Talha <talhaanwar.anwar@gmail.com>
2021-01-27 06:33:56 +0530

    Reverse a string also handle unicode using unicode_segmentation crate

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,6 +1,8 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
     let mut reversed_s = String::new();
-    for char in input.chars().rev() {
+    for char in input.graphemes(true).rev() {
         reversed_s.push_str(&char.to_string());
     }
     reversed_s
