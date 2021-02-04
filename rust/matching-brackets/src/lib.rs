e723835698ee08c207c367a3e9fd0e5980eec9f5
Talha <talhaanwar.anwar@gmail.com>
2021-02-05 04:42:02 +0530

    Given a string containing brackets [], braces {}, parentheses (), or any combination thereof, verify that any and all pairs are matched and nested correctly.

---
+++ rust/matching-brackets/src/lib.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+pub fn convert_option_to_value(option: Option<&char>) -> String {
+    match option {
+        Some(option) => option.to_string(),
+        None => "Not Found".to_string(),
+    }
+}
+
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let allowed_char: [char; 6] = ['[',']','{','}','(',')'];
+    let mut char_inverse_map_start: HashMap<char, char> = HashMap::new();
+	char_inverse_map_start.insert('[' , ']');
+	char_inverse_map_start.insert('{' , '}');
+    char_inverse_map_start.insert('(' , ')');
+    let mut stack = vec!['n'];
+    for char in string.chars() {
+        if allowed_char.iter().any(|&i| i==char) {
+            // check start
+            if char_inverse_map_start.contains_key(&char) {
+                // push to stack
+                stack.push(convert_option_to_value(char_inverse_map_start.get(&char)).chars().nth(0).unwrap());
+            } else {
+                let stack_top = convert_option_to_value(stack.last());
+                if stack_top != "Not Found" && stack_top.chars().nth(0).unwrap() == char {
+                    stack.pop();
+                } else {
+                    stack.push(convert_option_to_value(char_inverse_map_start.get(&char)).chars().nth(0).unwrap());
+                }
+            }
+        }
+    }
+    stack.len() == 1
+}
