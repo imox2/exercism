506554b157adf0de95841a9c7e39d5163fa7e565
Talha <talhaanwar.anwar@gmail.com>
2021-02-07 04:59:55 +0530

    High-score improved implementation

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,60 +1,38 @@
 #[derive(Debug)]
 pub struct HighScores {
-    score_list: Vec<u32>,
-    score_list_sorted: Vec<u32>,
+    scores: Vec<u32>,
 }
 
 impl HighScores {
     pub fn new(scores: &[u32]) -> Self {
-        let mut scores_vec = Vec::new();
-        for i in scores.iter() {
-            scores_vec.push(*i);
-        }
-        let mut scores_vec_copy = scores_vec.to_vec();
-        scores_vec_copy.sort();
-        scores_vec_copy.reverse();
-        HighScores {
-            score_list: scores_vec,
-            score_list_sorted: scores_vec_copy, 
+        Self {
+            scores: scores.to_vec(),
         }
     }
 
     pub fn scores(&self) -> &[u32] {
-        &self.score_list[..]
+        &self.scores[..]
     }
 
     pub fn latest(&self) -> Option<u32> {
-        match &self.score_list.last() {
-            Some(last) => Some(**last),
+        match self.scores.last() {
+            Some(last) => Some(*last),
             None => None,
         }
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        let mut max = 0;
-        for i in &self.score_list {
-            if i > &max {
-                max = *i;
-            }
-        }
-        if max == 0 {
-            return None 
-        } else { 
-            return Some(max) 
-        };
+        match self.scores.iter().max() {
+			Some(score) => Some(*score),
+			None => None,
+		}
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        let mut scores_vec = Vec::new();
-        let mut count = 0;
-        for i in &self.score_list_sorted {
-            if count == 3 {
-                break;
-            } else {
-                scores_vec.push(*i);
-                count += 1;
-            }
-        }
+        let mut scores_vec = self.scores().to_vec();
+        scores_vec.sort_unstable();
+        scores_vec.reverse();
+        scores_vec.truncate(3);
         scores_vec
     }
 }
